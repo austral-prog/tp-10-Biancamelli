d7e02d31fee8757ca36f509f6f9abaf4632ffbc9
Biancamelli <biancamelli249@gmail.com>
2024-10-23 21:29:49 -0300

    Update set_loops.py

--- set_loops.py
+++ set_loops.py
@@ -1,5 +1,6 @@
-def unique_character(words):
-    unique_chars=set(words)
-    return unique_chars
-   
+def unique_character(string):
+    st=set()
+    for i in string:
+        st.add(i)
+    return st
 print(unique_character("hello"))
